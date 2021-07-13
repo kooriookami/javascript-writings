46628acfe19c3b92f5a269e74fe4c05f5eb0d8cd
kooriookami <bingshuanglingluo@163.com>
2021-07-13 17:31:36 +0800

    feat(Q3): 更新第三题

--- codes/Q3/getStraightDotList.js
+++ codes/Q3/getStraightDotList.js
@@ -4,42 +4,25 @@ const getStraightDotList = (dot1, dot2) => {
     let x2 = dot2.x;
     let y2 = dot2.y;
 
-    const dotList = [];
-
     const k = x1 === x2 ? 0 : (y2 - y1) / (x2 - x1);
     const b = y1 - k * x1;
-    const fx = x =>  k * x + b;
-    const fy = y => k ? (y - b) / k : b;
-    const getDotList = () => [...new Set(dotList.map(v => JSON.stringify(v)))].map(v => JSON.parse(v));
+    const fx = x => y1 === y2 ? y1 : k * x + b;
+    const fy = y => x1 === x2 || !k ? x1 : (y - b) / k;
+
+    const dotList = [];
 
     if (x1 > x2) {
         [x1, x2] = [x2, x1];
     }
+    for (let x = x1; x <= x2; x++) {
+        dotList.push({x, y: fx(x) >= 0 ? Math.ceil(fx(x)) : Math.floor(fx(x))});
+    }
     if (y1 > y2) {
         [y1, y2] = [y2, y1];
     }
-    if (x1 === x2) {
-        for (let y = y1; y <= y2; y++) {
-            dotList.push({x: x1, y});
-        }
-        return getDotList();
-    } else {
-        for (let x = x1; x <= x2; x++) {
-            dotList.push({x, y: Math.ceil(fx(x))});
-        }
-    }
-    if (y1 === y2) {
-        for (let x = x1; x <= x2; x++) {
-            dotList.push({x, y: y1});
-        }
-        return getDotList();
-    } else {
-        for (let y = y1; y <= y2; y++) {
-            dotList.push({x: Math.ceil(fy(y)), y});
-        }
+    for (let y = y1; y <= y2; y++) {
+        dotList.push({x: fy(y) >= 0 ? Math.ceil(fy(y)) : Math.floor(fy(y)), y});
     }
 
-    return getDotList();
+    return [...new Set(dotList.map(v => JSON.stringify(v)))].map(v => JSON.parse(v));
 };
-
-console.log(getStraightDotList({x: 12, y: 2}, {x: 12, y: 3}));
